c22670b991f57f6fc0d4a3bf53b8ca630d6f122b
lynnwahh <81065298+lynnwahh@users.noreply.github.com>
2022-03-23 21:47:26 +0800

    Update trans2jpg.py

--- lung_code/trans_data/trans2jpg.py
+++ lung_code/trans_data/trans2jpg.py
@@ -7,7 +7,8 @@ import SimpleITK as sitk
 import numpy as np
 import tensorflow as tf
 import cv2
-result_path = '2.jpg'
+import pandas
+
 def trans(dcm_path, result_path):
     img = sitk.ReadImage(dcm_path)
     image_array = sitk.GetArrayFromImage(img)[0]
@@ -15,11 +16,27 @@ def trans(dcm_path, result_path):
     image_array = np.array([image_array for i in range(3)]).transpose(1,2,0)
     print(image_array.shape)
     img = image_array.astype(np.float64)
-    img = (img-np.min(img))/(np.max(img)-np.min(img))*255#*255,归一化
+    img = (img-np.min(img))/(np.max(img)-np.min(img))*255
     img = img.astype(int)
     cv2.imwrite(result_path)
 
-file_dir = './data'
+file_dir1 = './data_one'
+file_dir2 = './data_4in1'
+metadata = './dataset1_metadata.csv'
 re_dir = './re'
-for file in os.listdir(file_dir):
-    trans(os.path.join(file_dir, file), os.path.join(re_dir, file))
+df = pd.read_csv(metadata)
+for id in os.listdir(file_dir1):
+    id_dir = os.path.join(file_dir1, id)
+    cla = df.loc[df['patient id']==id].values[0][1]
+    cla_dir = os.path.join(re_dir, cla)
+    os.makedirs(cla_dir, exist_ok=True)
+    for file in os.listdir(id_dir):
+        trans(os.path.join(id_dir, file), os.path.join(cla_dir, file))
+ 
+for id in os.listdir(file_dir2):
+    id_dir = os.path.join(file_dir2, id)
+    cla = df.loc[df['patient id']==id].values[0][1]
+    cla_dir = os.path.join(re_dir, cla)
+    os.makedirs(cla_dir, exist_ok=True)
+    for file in os.listdir(id_dir):
+        trans(os.path.join(id_dir, file), os.path.join(cla_dir, file))
